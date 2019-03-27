8feac2d83ca3771e43554fa4760449965938111d
Tommaso Villa <tmms.vll@gmail.com>
2019-03-27 12:52:05 +0100

    Added Data teams

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+import { data } from './utils/TeamStats';
 
 import {
   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
-
-const data = [
-  { day: '15', Barcelona: 0, Betis: 0, Real: 0},
-  { day: '16', Barcelona: 3, Betis: 3, Real: 3 },
-  { day: '17', Barcelona: 6, Betis: 4, Real: 4 },
-  { day: '18', Barcelona: 9, Betis: 1, Real: 7 },
-  { day: '19', Barcelona: 12, Betis: -2, Real: 4 },
-  { day: '20', Barcelona: 15, Betis: 1, Real: 7 },
-  { day: '21', Barcelona: 18, Betis: -2, Real: 10},
-  { day: '22', Barcelona: 19, Betis: 1, Real: 11 },
-  { day: '23', Barcelona: 20, Betis: -2, Real: 14 },
-  { day: '24', Barcelona: 23, Betis: -1, Real: 15 },
-  { day: '25', Barcelona: 26, Betis: 2, Real: 12 },
-  { day: '26', Barcelona: 29, Betis: -1, Real: 13 },
-  { day: '27', Barcelona: 32, Betis: 2, Real: 16 },
-];
 
 const initialState = {
   data,
@@ -60,12 +45,22 @@ class App extends React.Component {
                 <div className="col-xs-7 form-container">
                 Pick your favorite Team:
                 <select value={this.state.value} onChange={this.handleChangeHomeTeam}>
+                  <option value="Atletico">Atletico</option>
+                  <option value="Barcelona">Barcelona</option>
                   <option value="Betis">Betis</option>
                   <option value="Real">Real</option>
+                  <option value="Sevilla">Sevilla</option>
+                  <option value="Valencia">Valencia</option>
+                  <option value="Villareal">Villareal</option>
                 </select>
                 <select value={this.state.value} onChange={this.handleChangeAwayTeam}>
+                  <option value="Atletico">Atletico</option>
+                  <option value="Barcelona">Barcelona</option>
                   <option value="Betis">Betis</option>
                   <option value="Real">Real</option>
+                  <option value="Sevilla">Sevilla</option>
+                  <option value="Valencia">Valencia</option>
+                  <option value="Villareal">Villareal</option>
                 </select>
                   <LineChart
                     width={500}
@@ -80,8 +75,7 @@ class App extends React.Component {
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    // <Line type="monotone" dataKey={this.state.value} stroke="#8884d8" activeDot={{ r: 8 }} />
-                    <Line type='monotone' dataKey={this.state.hometeamValue} stroke='#132908' activeDot={{fill: '#132908', stroke: 'none', r: 6}}/>
+                    <Line type='monotone' dataKey={this.state.hometeamValue} stroke='#c60000' activeDot={{fill: '#132908', stroke: 'none', r: 6}}/>
                     <Line type='monotone' dataKey={this.state.awayteamValue} stroke='#132908' activeDot={{fill: '#132908', stroke: 'none', r: 6}}/>
                   </LineChart>
                 </div>
---
+++ src/utils/TeamStats.js
@@ -0,0 +1,16 @@
+export const data = [
+  { day: '15', Atletico: 0, Barcelona: 0, Betis: 0, Real: 0, Sevilla: 0, Valencia: 0, Villareal: 0},
+  { day: '16', Atletico: 2, Barcelona: 2, Betis: 2, Real: 0, Sevilla: 0, Valencia: 0, Villareal: 0},
+  { day: '17', Atletico: 2, Barcelona: 2, Betis: 0, Real: 0, Sevilla: 0, Valencia: 0, Villareal: -2},
+  { day: '18', Atletico: 2, Barcelona: 4, Betis: -1, Real: -3 , Sevilla: -2, Valencia: -1, Villareal: -2},
+  { day: '19', Atletico: 2, Barcelona: 4, Betis: -4, Real: -1, Sevilla: -3, Valencia: -3, Villareal: -5},
+  { day: '20', Atletico: 4, Barcelona: 4, Betis: -4, Real: -1, Sevilla: -4, Valencia: -1, Villareal: -7},
+  { day: '21', Atletico: 4, Barcelona: 6, Betis: -5, Real:  1, Sevilla: -4, Valencia: -1, Villareal: -8},
+  { day: '22', Atletico: 3, Barcelona: 4, Betis: -5, Real: 1, Sevilla: -5, Valencia: -1, Villareal: -10},
+  { day: '23', Atletico: 0, Barcelona: 4, Betis: -5, Real: 3 , Sevilla: -7, Valencia: -3, Villareal: -10},
+  { day: '24', Atletico: 2, Barcelona: 4, Betis: -7, Real: 0, Sevilla: -8, Valencia: -5, Villareal: -10},
+  { day: '25', Atletico: 2, Barcelona: 6, Betis: -5, Real: 2, Sevilla: -11, Valencia: -5, Villareal: -11},
+  { day: '26', Atletico: 4, Barcelona: 8, Betis: -8, Real: -1, Sevilla: -12, Valencia: -5, Villareal: -14},
+  { day: '27', Atletico: 4, Barcelona: 8, Betis: -6, Real: 1, Sevilla: -12, Valencia: -3, Villareal: -12},
+  { day: '28', Atletico: 3, Barcelona: 10, Betis: -9, Real: 1, Sevilla: -10, Valencia: -5, Villareal: -12},
+];
