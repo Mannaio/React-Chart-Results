82178972577f4e19cf5de2d8e1e6ad1cc79cb418
Tommaso Villa <tmms.vll@gmail.com>
2019-03-30 10:17:55 +0100

    Added Team Data Fixtures

--- src/utils/TeamStats.js
+++ src/utils/TeamStats.js
@@ -1,18 +1,18 @@
-export const liga = ['Atletico', 'Barcelona', 'Betis', 'Real', 'Sevilla', 'Valencia', 'Villareal'];
+export const liga = ['Atletico', 'Barcelona', 'Betis', 'Espanyol', 'Real', 'Sevilla', 'Valencia', 'Valladolid', 'Villareal'];
 
 export const data = [
-  { day: '15', Atletico: 0, Barcelona: 0, Betis: 0, Real: 0, Sevilla: 0, Valencia: 0, Villareal: 0},
-  { day: '16', Atletico: 2, Barcelona: 2, Betis: 2, Real: 0, Sevilla: 0, Valencia: 0, Villareal: 0},
-  { day: '17', Atletico: 2, Barcelona: 2, Betis: 0, Real: 0, Sevilla: 0, Valencia: 0, Villareal: -2},
-  { day: '18', Atletico: 2, Barcelona: 4, Betis: -1, Real: -3 , Sevilla: -2, Valencia: -1, Villareal: -2},
-  { day: '19', Atletico: 2, Barcelona: 4, Betis: -4, Real: -1, Sevilla: -3, Valencia: -3, Villareal: -5},
-  { day: '20', Atletico: 4, Barcelona: 4, Betis: -4, Real: -1, Sevilla: -4, Valencia: -1, Villareal: -7},
-  { day: '21', Atletico: 4, Barcelona: 6, Betis: -5, Real:  1, Sevilla: -4, Valencia: -1, Villareal: -8},
-  { day: '22', Atletico: 3, Barcelona: 4, Betis: -5, Real: 1, Sevilla: -5, Valencia: -1, Villareal: -10},
-  { day: '23', Atletico: 0, Barcelona: 4, Betis: -5, Real: 3 , Sevilla: -7, Valencia: -3, Villareal: -10},
-  { day: '24', Atletico: 2, Barcelona: 4, Betis: -7, Real: 0, Sevilla: -8, Valencia: -5, Villareal: -10},
-  { day: '25', Atletico: 2, Barcelona: 6, Betis: -5, Real: 2, Sevilla: -11, Valencia: -5, Villareal: -11},
-  { day: '26', Atletico: 4, Barcelona: 8, Betis: -8, Real: -1, Sevilla: -12, Valencia: -5, Villareal: -14},
-  { day: '27', Atletico: 4, Barcelona: 8, Betis: -6, Real: 1, Sevilla: -12, Valencia: -3, Villareal: -12},
-  { day: '28', Atletico: 3, Barcelona: 10, Betis: -9, Real: 1, Sevilla: -10, Valencia: -5, Villareal: -12},
+  { day: '15', Atletico: 0, Barcelona: 0, Betis: 0, Espanyol: 0, Real: 0, Sevilla: 0, Valladolid: 0, Valencia: 0, Villareal: 0},
+  { day: '16', Atletico: 2, Barcelona: 2, Betis: 2, Espanyol: -3, Real: 0, Sevilla: 0, Valladolid: -3, Valencia: 0, Villareal: 0},
+  { day: '17', Atletico: 2, Barcelona: 2, Betis: 0, Espanyol: -4, Real: 0, Sevilla: 0, Valladolid: -3, Valencia: 0, Villareal: -2},
+  { day: '18', Atletico: 2, Barcelona: 4, Betis: -1, Espanyol: -4, Real: -3 , Sevilla: -2, Valladolid: -6, Valencia: -1, Villareal: -2},
+  { day: '19', Atletico: 2, Barcelona: 4, Betis: -4, Espanyol: -5, Real: -1, Sevilla: -3, Valladolid: -6, Valencia: -3, Villareal: -5},
+  { day: '20', Atletico: 4, Barcelona: 4, Betis: -4, Espanyol: -6, Real: -1, Sevilla: -4, Valladolid: -7, Valencia: -1, Villareal: -7},
+  { day: '21', Atletico: 4, Barcelona: 6, Betis: -5, Espanyol: -9, Real:  1, Sevilla: -4, Valladolid: -7, Valencia: -1, Villareal: -8},
+  { day: '22', Atletico: 3, Barcelona: 4, Betis: -5, Espanyol: -9, Real: 1, Sevilla: -5, Valladolid: -8, Valencia: -1, Villareal: -10},
+  { day: '23', Atletico: 0, Barcelona: 4, Betis: -5, Espanyol: -9, Real: 3 , Sevilla: -7, Valladolid: -10, Valencia: -3, Villareal: -10},
+  { day: '24', Atletico: 2, Barcelona: 4, Betis: -7, Espanyol: -9, Real: 0, Sevilla: -8, Valladolid: -11, Valencia: -5, Villareal: -10},
+  { day: '25', Atletico: 2, Barcelona: 6, Betis: -5, Espanyol: -11, Real: 2, Sevilla: -11, Valladolid: -14, Valencia: -5, Villareal: -11},
+  { day: '26', Atletico: 4, Barcelona: 8, Betis: -8, Espanyol: -11, Real: -1, Sevilla: -12, Valladolid: -15, Valencia: -5, Villareal: -14},
+  { day: '27', Atletico: 4, Barcelona: 8, Betis: -6, Espanyol: -11, Real: 1, Sevilla: -12, Valladolid: -18, Valencia: -3, Villareal: -12},
+  { day: '28', Atletico: 3, Barcelona: 10, Betis: -9, Espanyol: -14, Real: 1, Sevilla: -10, Valladolid: -15, Valencia: -5, Villareal: -12},
 ];
