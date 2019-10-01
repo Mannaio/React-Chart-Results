bec4cc13356b24f29a647f5692e550d7daf1e1a9
Tommaso Villa <tmms.vll@gmail.com>
2019-10-01 17:18:12 +0200

    Small Text Changes

--- src/App.js
+++ src/App.js
@@ -67,7 +67,8 @@ class App extends React.Component {
                     <p>Description: The trend is established using the <a href="https://it.wikipedia.org/wiki/Media_inglese" target="blank">English Average calculation</a></p>.
                     <Table />
                     <p>There are three things taken in consideration, the team playing at home, the team playing away and their english average. Based on that we can better predict the matches, gambling on the result which gives a closer score to the average team.
-                    It is also important to consider looking at the team charts on the right that like in trade we are introducing some concepts as 'support' and 'resistance'. Any team on his way has to face this. What does it mean? This means that we also have to take in consideration that the teams have to rest or react at some stages. This can be seen in the charts as an horizontal / flat step before going up or down again.</p>
+                    It is also important to consider looking at the team charts on the right that like in trade we are introducing some concepts as 'support', 'resistance' and 'consolidation'. Any team on his way has to face this. What does it mean? This means that we also have to take in consideration that the teams have to rest or react at some stages. This can be seen in the charts as an horizontal / flat step before going up or down again.</p>
+                    <h3>Tips</h3>
                   </div>
                 </div>
                 <div className="col-xs-7 form-container">
