2099d50bb44276e23c6fadf0de1bffd97be9d700
Tommaso Villa <tmms.vll@gmail.com>
2019-10-23 12:20:58 +0200

    Average Team Score for All the Leagues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { scores, scoresseriea } from './utils/TeamStats';
+import { scores } from './utils/TeamStats';
 import Table from "./components/Table";
 
 import {
@@ -10,6 +10,7 @@ const data = {
   Liga: ['Alaves','Atletico', 'Barcelona', 'Betis', 'Bilbao', 'Celta', 'Espanyol', 'Getafe', 'Granada', 'Leganes', 'Levante', 'Eibar', 'Mallorca', 'Osasuna', 'Real', 'Sevilla', 'Sociedad', 'Valencia', 'Valladolid', 'Villareal'],
   Seriea: ['Atalanta', 'Bologna', 'Brescia', 'Cagliari', 'Fiorentina', 'Genoa', 'Inter', 'Juventus', 'Lazio', 'Lecce', 'Milan', 'Napoli', 'Parma', 'Roma', 'Sampdoria', 'Sassuolo', 'Spal', 'Torino', 'Udinese', 'Verona']
 };
+
 
 const leagues = Object.keys(data);
 
@@ -24,37 +25,35 @@ const getTeams = league =>
       ))
     : [];
 
-const getAverage = team => {
+const getAverage = (league, team) => {
 
-  if (isNaN(scoresseriea[0][team])) return null;
+  if (isNaN(league[0][team])) return null;
 
-  return scoresseriea.map(x => x[team]).reduce((a, c) => a + c) / scoresseriea.length;
-};
-
-const maxAverage = teams => {
-  return teams.map(team => {return { team:team, avg: getAverage(team)}}).reduce((a,b)=>a.avg>b.avg?a:b).team
+  return league.map(x => x[team]).reduce((a, c) => a + c) / league.length;
 };
 
 
 class App extends React.Component {
   state = {
     selectedLeague: leagues[0],
-    selectedTeam: getTeams(leagues[0])[0],
-    scoresseriea,
-    homeTeam: '',
-    awayTeam: ''
+    selectedHomeTeam: getTeams(leagues[0])[0],
+    selectedAwayTeam: getTeams(leagues[0])[0],
+    scores
   };
 
   handleLeagueChange = event => {
-    this.setState({ selectedLeague: event.target.value });
+    const selectedLeague = event.target.value;
+    this.setState({ selectedLeague: selectedLeague }, () => {
+      this.setState({ selectedHomeTeam: data[selectedLeague][0],selectedAwayTeam: data[selectedLeague][0] });
+    });
   };
 
   handleHomeTeamChange = event => {
-    this.setState({ homeTeam: event.target.value });
+    this.setState({ selectedHomeTeam: event.target.value });
   };
 
   handleAwayTeamChange = event => {
-    this.setState({ awayTeam: event.target.value });
+    this.setState({ selectedAwayTeam: event.target.value });
   };
 
   render() {
@@ -71,12 +70,11 @@ class App extends React.Component {
                 		<h3 className="title-container__subtitle">Your best tool to predict the matches results</h3>
                   </div>
                   <div>
-                    <p>Home Team: <span className="strong-red">{this.state.homeTeam}</span></p>
-                    <p>Away Team: <span className="strong-blue">{this.state.awayTeam}</span></p>
-                    <p>Average {this.state.homeTeam}: <span className="strong">{getAverage(this.state.homeTeam)}</span></p>
-                    <p>Average {this.state.awayTeam}: <span className="strong">{getAverage(this.state.awayTeam)}</span></p>
-                    <p>Stronger Team: <span className="strong-red">{maxAverage([this.state.homeTeam,this.state.awayTeam])}</span></p>
-                    <p>Description: The trend is established using the <a href="https://it.wikipedia.org/wiki/Media_inglese" target="blank">English Average calculation</a></p>.
+                    <p>Home Team: <span className="strong-red">{this.state.selectedHomeTeam}</span></p>
+                    <p>Away Team: <span className="strong-blue">{this.state.selectedAwayTeam}</span></p>
+                    <p>Average {this.state.selectedHomeTeam}: <span className="strong">{getAverage(scores[this.state.selectedLeague], this.state.selectedHomeTeam)}</span></p>
+                    <p>Average {this.state.selectedAwayTeam}: <span className="strong">{getAverage(scores[this.state.selectedLeague], this.state.selectedAwayTeam)}</span></p>
+                    <p>Description: The trend is established using the <a href="https://it.wikipedia.org/wiki/Media_inglese" target="blank">English Average calculation</a></p>
                     <Table />
                     <h3>Description</h3>
                     <p>There are three things taken in consideration, the team playing at home, the team playing away and their english average. Based on that we can better predict the matches, gambling on the result which gives a closer score to the average team.
@@ -94,14 +92,14 @@ class App extends React.Component {
                 </select>
                 Pick the Home Team
                 <select
-                  value={this.state.homeTeam}
+                  value={this.state.selectedHomeTeam}
                   onChange={this.handleHomeTeamChange}
                 >
                   {getTeams(this.state.selectedLeague)}
                 </select>
                 Pick the Away Team
                 <select
-                  value={this.state.awayTeam}
+                  value={this.state.selectedAwayTeam}
                   onChange={this.handleAwayTeamChange}
                 >
                   {getTeams(this.state.selectedLeague)}
@@ -109,7 +107,7 @@ class App extends React.Component {
                   <LineChart
                     width={500}
                     height={300}
-                    data={scoresseriea}
+                    data={scores[this.state.selectedLeague]}
                     margin={{
                       top: 5, right: 30, left: 20, bottom: 5,
                     }}
@@ -119,8 +117,8 @@ class App extends React.Component {
                     <YAxis />
                     <Tooltip />
                     <Legend />
-                    <Line type='monotone' dataKey={this.state.homeTeam} stroke='#c60000' activeDot={{fill: '#c60000', stroke: 'none', r: 6}}/>
-                    <Line type='monotone' dataKey={this.state.awayTeam} stroke='#132908' activeDot={{fill: '#132908', stroke: 'none', r: 6}}/>
+                    <Line type='monotone' dataKey={this.state.selectedHomeTeam} stroke='#c60000' activeDot={{fill: '#c60000', stroke: 'none', r: 6}}/>
+                    <Line type='monotone' dataKey={this.state.selectedAwayTeam} stroke='#132908' activeDot={{fill: '#132908', stroke: 'none', r: 6}}/>
                   </LineChart>
                 </div>
               </div>
