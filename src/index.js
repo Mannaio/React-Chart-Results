2330f7fbddefef2fb437916fd390dec36e61eb5c
Tommaso Villa <tmms.vll@gmail.com>
2019-11-05 08:47:25 +0100

    Small CSS Customized framework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css";
+// import "./App.css";
+import './css/Main.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
